018b911fa9c1013b16f73831dbe5ed1a1f0b3c82
Ian Laidlaw <ilaidlaw@radford.edu>
2025-02-25 14:42:01 -0500

    search bar functionality

--- src/lib/components/AppSidebar.svelte
+++ src/lib/components/AppSidebar.svelte
@@ -2,7 +2,18 @@
 	import * as Sidebar from "$lib/components/ui/sidebar/index.js";
 	import Svelecte from "svelecte";
 
-	let value: string;
+	let options = [
+		"First",
+		"Second",
+		"Third",
+		"Fourth",
+		"Fifth",
+		"Sixth",
+		"Seventh",
+		"Eight",
+		"Ninth",
+		"Tenth",
+	];
 </script>
 
 <Sidebar.Root
@@ -14,8 +25,25 @@
 			<Sidebar.GroupLabel>Search</Sidebar.GroupLabel>
 			<Sidebar.GroupContent>
 				<Sidebar.Menu>
-					<Svelecte options={["One", "Two", "Three"]} bind:value />
+					<Svelecte
+						searchable
+						clearable
+						multiple
+						allowEditing
+						keepCreated
+						placeholder="Search Tags"
+						{options}
+					/>
 				</Sidebar.Menu>
+			</Sidebar.GroupContent>
+		</Sidebar.Group>
+		<Sidebar.Separator />
+		<Sidebar.Group>
+			<Sidebar.GroupLabel>Tags</Sidebar.GroupLabel>
+			<Sidebar.GroupContent class="flex flex-wrap">
+				{#each options as option}
+					<div class="tag">option</div>
+				{/each}
 			</Sidebar.GroupContent>
 		</Sidebar.Group>
 	</Sidebar.Content>

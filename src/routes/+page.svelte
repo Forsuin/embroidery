34ce425fa83f524c89a5b0d988f4d98226e38321
Ian Laidlaw <ilaidlaw@radford.edu>
2025-02-26 17:33:52 -0500

    Added grid of cards for content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,7 @@
   import AppSidebar from "$lib/components/AppSidebar.svelte";
   import * as Resizable from "$lib/components/ui/resizable";
   import { ScrollArea } from "$lib/components/ui/scroll-area";
+  import * as Card from "$lib/components/ui/card";
 
   const tags = Array.from({ length: 50 }).map(
     (_, i, a) => `v1.2.0-beta.${a.length - i}`,
@@ -23,12 +24,25 @@
         <Resizable.Handle />
         <Resizable.Pane defaultSize={70}>
           <Sidebar.Inset>
-            <ScrollArea class="h-lvh min-w-lg p-4 border ">
-              <div class="p-4">
-                <h4 class="mb-4 text-sm font-medium leading-none">Tags</h4>
+            <ScrollArea class="h-lvh min-w-full p-4 border">
+              <div
+                class="grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))] gap-4"
+              >
                 {#each tags as tag}
-                  <div class="text-sm">
-                    {tag}
+                  <div>
+                    <Card.Root>
+                      <Card.Header>
+                        <Card.Title
+                          >Title: This is a really long title, hopefully this
+                          will make the cards bigger so I can see bigger ones</Card.Title
+                        >
+                        <Card.Description>Description</Card.Description>
+                      </Card.Header>
+                      <Card.Content>
+                        {tag}
+                      </Card.Content>
+                      <Card.Footer>Footer</Card.Footer>
+                    </Card.Root>
                   </div>
                 {/each}
               </div>

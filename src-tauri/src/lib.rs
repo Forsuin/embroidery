aab3c9acdbcddfd75812a6835295c11af66fd0fc
Ian Laidlaw <ilaidlaw@radford.edu>
2025-03-06 16:25:30 -0500

    removed menu creation from backend

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,4 @@
-use tauri::{
-    menu::{MenuBuilder, MenuItem, MenuItemBuilder, SubmenuBuilder},
-    Listener,
-};
+use tauri::Listener;
 
 #[tauri::command]
 fn format_search_query(args: Vec<String>) -> String {
@@ -13,64 +10,6 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_os::init())
         .setup(|app| {
-            let _handle = app.handle();
-
-            let file_explorer_prompt = if cfg!(target_os = "windows") {
-                "Reveal in File Explorer"
-            } else if cfg!(target_os = "macos") {
-                "Show in Finder"
-            } else {
-                "Show in File Manager"
-            };
-
-            let file_submenu = SubmenuBuilder::new(app, "File")
-                .item(
-                    &MenuItemBuilder::new("Import")
-                        .id("import")
-                        .accelerator("CmdOrCtrl+I")
-                        .build(app)?,
-                )
-                .item(
-                    &MenuItemBuilder::new(file_explorer_prompt)
-                        .id("reveal")
-                        .accelerator("CmdOrCtrl+Alt+R")
-                        .build(app)?,
-                )
-                .separator()
-                .item(
-                    &MenuItemBuilder::new("Settings")
-                        .id("settings")
-                        .accelerator("CmdOrCtrl+,")
-                        .build(app)?,
-                )
-                .separator()
-                .item(
-                    &MenuItemBuilder::new("Quit")
-                        .id("quit")
-                        .accelerator("CmdOrCtrl+q")
-                        .build(app)?,
-                )
-                .build()?;
-
-            let edit_submenu = SubmenuBuilder::new(app, "Edit")
-                .items(&[
-                    &MenuItemBuilder::new("Undo".to_string())
-                        .id("undo")
-                        .accelerator("CmdOrCtrl+Z")
-                        .build(app)?,
-                    &MenuItemBuilder::new("Redo")
-                        .id("redo")
-                        .accelerator("CmdOrCtrl+Shift+Z")
-                        .build(app)?,
-                ])
-                .build()?;
-
-            let menu = MenuBuilder::new(app)
-                .items(&[&file_submenu, &edit_submenu])
-                .build()?;
-
-            app.set_menu(menu)?;
-
             app.on_menu_event(move |_app_handle: &tauri::AppHandle, event| {
                 println!("Event: {:?}", event);
             });
